98b247fd37caf31f62817610e22b0b583316188d
sami-p <sami.peterson14@gmail.com>
2021-04-20 14:44:35 -0500

    Test that Item instance has a price with item2 as test subject

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe Item do
 
       expect(item2.name).to eq('Apple Pie (Slice)')
     end
+
+    it 'has a price' do
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(item2.price).to eq('$2.50')
+    end
   end
 end
