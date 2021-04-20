84675418d73f28c41e6b1809661357e9047700d5
sami-p <sami.peterson14@gmail.com>
2021-04-20 14:17:08 -0500

    Test that it has name upon initialization

--- spec/foodtruck_spec.rb
+++ spec/foodtruck_spec.rb
@@ -4,6 +4,15 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
   describe '#initialize' do
     it 'exists' do
-    end 
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck).to be_an_instance_of(FoodTruck)
+    end
+
+    it 'has a name' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
+    end
   end
 end
