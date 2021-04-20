85b71dee84b6ec407db4ddfaa663164b6df1bc62
sami-p <sami.peterson14@gmail.com>
2021-04-20 14:22:58 -0500

    Test for initial #inventory hashh

--- spec/foodtruck_spec.rb
+++ spec/foodtruck_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe FoodTruck do
       expect(food_truck.name).to eq("Rocky Mountain Pies")
     end
   end
+
+  describe '#inventory' do
+    it 'starts with empty inventory hash' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck.inventory).to eq({})
+    end
+  end
 end
