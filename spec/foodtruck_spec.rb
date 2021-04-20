4e6074c1f68acd5baba06ba3993db0faa8e12cfc
sami-p <sami.peterson14@gmail.com>
2021-04-20 15:20:25 -0500

    Test #stock method

--- spec/foodtruck_spec.rb
+++ spec/foodtruck_spec.rb
@@ -29,8 +29,30 @@ RSpec.describe FoodTruck do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-      
+
       expect(food_truck.check_stock(item1)).to eq(0)
+    end
+  end
+
+  describe '#stock' do
+    it 'stocks specified # of items and updates inventory' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.check_stock(item1)).to eq(30)
+      expect(food_truck.inventory).to eq({item1 => 30})
+    end
+
+    it 'can stock more than one item multiple times' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      food_truck.stock(item1, 30)
+      food_truck.stock(item1, 25)
+      food_truck.stock(item2, 12)
     end
   end
 end
