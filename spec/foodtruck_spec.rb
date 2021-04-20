c5e5c2cd23e59c08a671dd9d2e2342ef48589a22
sami-p <sami.peterson14@gmail.com>
2021-04-20 14:41:29 -0500

    Modify and pass #check_stock based on argument

--- spec/foodtruck_spec.rb
+++ spec/foodtruck_spec.rb
@@ -27,8 +27,10 @@ RSpec.describe FoodTruck do
   describe '#check_stock' do
     it 'starts with 0 items in stock' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
-
-      expect(food_truck.check_stock).to eq(0)
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      
+      expect(food_truck.check_stock(item1)).to eq(0)
     end
   end
 end
